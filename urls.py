aef1577dea44fa7e8e054471990ab639f8753bb4
Playerz21 <86472636+Playerz21@users.noreply.github.com>
2021-09-07 10:33:55 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index
-
-urlpatterns = [
-    path('',index),
-    path('create/',index),
-    path('list/',index),
-    path('add/<str:code>',index),
-]
